905475022cee489a8750b500ef36a7751bb58f03
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2025-11-11 16:03:20 -0300

    Update app.py

--- first_steps/app.py
+++ first_steps/app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>teste</p>"
+
+
+
+# carregue o código:
+# com python -m flask run
+# Instalação:
+# pip install Flask
+
+# Ambiente Virtual: 
+# mkdir myproject
+# cd myproject
+# python3 -m venv .venv
